a04c78a22729a239ebc338c27f7d401428e05b29
David Voelkel <der.voelkel.david@gmail.com>
2015-03-03 00:28:35 +0100

    moved to ansible

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my-machine
 Provisions my linux machine with my required dev env &amp; dev ops tools
+
+Open terminal and type
+'''sudo aptitude install curl
+curl -s https://raw.githubusercontent.com/davidvoelkel/my-machine/master/bootstrap.sh | sudo bash'''
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-aptitude install vim curl git-core vagrant virtualbox virtualbox-dkms ansible
+aptitude install curl git-core 
+
+BASE_DIR=/tmp/my-machine
+git clone https://github.com/davidvoelkel/my-machine $BASE_DIR
+
+# Ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+$BASE_DIR/ansible-on-localhost.sh
